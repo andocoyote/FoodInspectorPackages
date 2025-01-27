628b8aa37bb28a8b9ca0563b7dbd704673693e9e
Andrew Mikesell <andocoyote@outlook.com>
2025-01-26 17:59:17 -0800

    Adding EstablishmentRecommendations model

---
+++ FoodInspectorModels/EstablishmentRecommendations.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoodInspectorModels
+{
+    public class EstablishmentRecommendations
+    {
+        [JsonPropertyName("recommended")]
+        public List<InspectionRecordAggregated>? Recommended { get; set; }
+
+        [JsonPropertyName("unrecommended")]
+        public List<InspectionRecordAggregated>? Unrecommended { get; set; }
+    }
+
+}
